c5ab5cfdc457b8a05c02defefd926d3153d5ab06
Tequila Ridgeway <kelaridgeway@gmail.com>
2024-10-23 16:54:29 -0400

    Created classes and variables

---
+++ car-dealership/src/main/java/com/pluralsight/dealership/Program.java
@@ -0,0 +1,7 @@
+package com.pluralsight.dealership;
+
+public class Program {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ car-dealership/src/main/java/com/pluralsight/dealership/UserInterface.java
@@ -0,0 +1,7 @@
+package com.pluralsight.dealership;
+
+import java.util.Scanner;
+public class UserInterface {
+    private Dealership dealership;
+    private Scanner scanner;
+}
