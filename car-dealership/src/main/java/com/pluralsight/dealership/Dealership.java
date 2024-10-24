1a6c57b8073ef4da9765a492fc6b0210885a5a8f
Tequila Ridgeway <kelaridgeway@gmail.com>
2024-10-24 11:55:51 -0400

    Added constructor to dealership class

--- car-dealership/src/main/java/com/pluralsight/dealership/Dealership.java
+++ car-dealership/src/main/java/com/pluralsight/dealership/Dealership.java
@@ -6,5 +6,13 @@ public class Dealership {
     private String name;
     private String address;
     private String phone;
-    private ArrayList<Vehicle> inventory;
+    private ArrayList<Vehicle> inventory; //(List of all the vehicles in dealership)
+
+    // Constructor
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>(); //Initiate the Arraylist
+    }
 }
