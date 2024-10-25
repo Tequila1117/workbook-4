0e9c44e73f765a2343af95a5d57c58fc766023a6
Tequila Ridgeway <kelaridgeway@gmail.com>
2024-10-25 15:00:29 -0400

    declared method to get vehicles by price range

--- car-dealership/src/main/java/com/pluralsight/dealership/Dealership.java
+++ car-dealership/src/main/java/com/pluralsight/dealership/Dealership.java
@@ -16,14 +16,27 @@ public class Dealership {
         this.phone = phone;
         this.inventory = new ArrayList<>(); //Initiate the Arraylist
     }
+
     // Add a vehicle to the inventory
     public void addVehicle(Vehicle vehicle) {
         inventory.add(vehicle);
     }
+
     // Get all vehicles in the inventory
     public List<Vehicle> getAllVehicles() {
         return inventory;
     }
 
+    // Get vehicles by price range
+    public List<Vehicle> getVehiclesByPrice(double min, double max) {
+        List<Vehicle> result = new ArrayList<>();
+        for (Vehicle v : inventory) {
+            if (v.getPrice() >= min && v.getPrice() <= max) {
+                result.add(v);
+            }
+        }
+        return result;
+
     }
+}
 
